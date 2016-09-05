5b79cb22683a48ba381b76e12f9e8ece6f8078bb
root <root@5b977d735eaa.(none)>
2016-09-05 06:06:23 +0000

    setup and teardown for testing init scripts

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import testinfra
 import socket
+import os
 
 backend = testinfra.get_backend('local://', connection='local')
 
@@ -16,3 +17,24 @@ def get_host_ipaddress(request):
 @pytest.fixture(scope='module')
 def TestinfraBackend(request):
     return backend
+
+def service_status(name):
+    return os.system('/sbin/service {0} status'.format(name))
+
+@pytest.fixture()
+def Start_Service(request):
+    def run(args):
+        status_rc = service_status(args)
+        if status_rc != 0:
+            return os.system('/sbin/service {0} start'.format(args))
+        return status_rc
+    return run
+
+@pytest.fixture()
+def Stop_Service(request):
+    def run(args):
+        status_rc = service_status(args)
+        if status_rc == 0:
+            return os.system('/sbin/service {0} stop'.format(args))
+        return status_rc
+    return run
--- tests/test_service.py
+++ tests/test_service.py
@@ -1,5 +1,23 @@
 import pytest
 import testinfra
+import time
+
+def assert_service_starts(Start_Service, Stop_Service, Service, name):
+    rc = Start_Service(name)
+    assert rc == 0
+
+    print('sleeping to make sure service has a chance to startup')
+    time.sleep(5)
+
+    svc = Service(name)
+    assert svc.is_running
+
+
+    rc = Stop_Service(name)
+    assert rc == 0
+
+    svc = Service(name)
+    assert svc.is_running == False
 
 @pytest.mark.parametrize('name', [
     'storm-nimbus',
@@ -8,6 +26,5 @@ import testinfra
     'storm-supervisor',
     'storm-ui',
 ])
-def test_service_starts_up(LocalCommand, Service, name):
-    svc = Service(name)
-    assert svc.is_running
+def test_service_starts_up(Start_Service, Stop_Service, Service, name):
+    assert_service_starts(Start_Service, Stop_Service, Service, name)
